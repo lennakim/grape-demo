704ae5c4defc20a67e4cff4793227321ae72f239
shooter <shooter.hero@gmail.com>
2015-06-25 17:01:45 +0800

    post method 202

--- app/api/v1/examples.rb
+++ app/api/v1/examples.rb
@@ -11,6 +11,12 @@ module V1
         {method: :post, extends: "post status 默认 201" }
       end
 
+      desc "post status 202"
+      post "post_202" do
+        status 202
+
+        wrapper({status: 202})
+      end
     end
   end
 end
