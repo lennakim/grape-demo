ed74e171e14ae32ce8adbe67b31742ddf49587af
shooter <shooter.hero@gmail.com>
2015-06-25 18:53:12 +0800

    Provinces list api

---
+++ app/api/v1/provinces.rb
@@ -0,0 +1,12 @@
+module V1
+  class Provinces < Grape::API
+    helpers V1::SharedParams
+
+    resources :provinces do
+      desc "Provinces list"
+      get "" do
+        wrapper(Province.all)
+      end
+    end
+  end
+end
