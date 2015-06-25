bb16f3e862f39051267f6786c315ac55150d292e
shooter <shooter.hero@gmail.com>
2015-06-25 15:35:06 +0800

    生成 doc controller

---
+++ app/controllers/doc_controller.rb
@@ -0,0 +1,11 @@
+class DocController < ApplicationController
+
+  layout "api"
+
+  def v1
+  end
+
+  def v2
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'doc/v2'
+
+  get 'doc/v1'
+
   get 'home/index'
 
   root "home#index"
